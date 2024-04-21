0cb584a06d0373d4cbe264b43cd451e5bffcf9d2
jofbrvega <jofbr.business@gmail.com>
2024-04-21 21:24:15 +0200

    Removed errors

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -2,7 +2,6 @@ import Avatar from "./avatar";
 import Date from "./date";
 import CoverImage from "./cover-image";
 import Link from "next/link";
-import exp from "constants";
 
 export default function HeroPost({
   title,
@@ -13,32 +12,32 @@ export default function HeroPost({
   slug,
 }) {
   return (
-    <Link href={`/posts/${slug}`} className="card w-full h-full glass">
-      <figure>
-        <img
-          className={"w-full object-cover"}
-          src={coverImage.node.sourceUrl}
-          alt={title}
-          title="{title}"
-        />
-      </figure>
-      <div className="card-body">
-        <div className={'flex justify-between'}>
-          <div>
-            <h2 className="card-title">{title}</h2>
-            <span className="mb-4 md:mb-0 text-lg">
-              <Date dateString={date} />
-            </span>
+    <Link href={`/posts/${slug}`} passHref>
+      <a className="card w-full h-full glass">
+        <figure>
+          <img
+            className={"w-full object-cover"}
+            src={coverImage.node.sourceUrl}
+            alt={title}
+            title="{title}"
+          />
+        </figure>
+        <div className="card-body">
+          <div className={"flex justify-between"}>
+            <div>
+              <h2 className="card-title">{title}</h2>
+              <span className="mb-4 md:mb-0 text-lg">
+                <Date dateString={date} />
+              </span>
+            </div>
+            <Avatar author={author} />
           </div>
-          <Avatar author={author} />
+          <div dangerouslySetInnerHTML={{ __html: excerpt }}></div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Learn now!</button>
+          </div>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: excerpt}}></div>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary" href={`/posts/${slug}`}>
-            Learn now!
-          </button>
-        </div>
-      </div>
+      </a>
     </Link>
   );
 }
